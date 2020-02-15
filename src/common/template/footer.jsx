fbe76fd4401f21a281d8075d63c2ad3d8502e522
wslmacieira <wslmacieira@outlook.com>
2020-02-15 17:29:04 -0200

    Componente Footer

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+  <footer className="main-footer">
+    <strong>
+      Copyright &copy; 2020
+      <a href="https://wslmacieira.netlify.com" target='_blank'> DW2L</a>
+    </strong>
+  </footer>
+)
--- src/main/app.jsx
+++ src/main/app.jsx
@@ -4,11 +4,16 @@ import '../common/template/dependencies'
 
 import Header from '../common/template/header'
 import Sidebar from '../common/template/sidebar'
+import Footer from '../common/template/footer'
 
 
 export default props => (
   <div className="wrapper">
     <Header icon='money' title='Money' />
     <Sidebar />
+    <div className="content-wrapper">
+      <h1>Conteudo</h1>
+    </div>
+    <Footer />
   </div>
 )
