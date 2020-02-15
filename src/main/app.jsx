a71b60cb63f5ca5bd6bebf4ead633acfc3772ee3
wslmacieira <wslmacieira@outlook.com>
2020-02-15 16:35:16 -0200

    Componente Header

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,10 +1,12 @@
-import '../common/template/dependencies'
 import React from 'react';
 
+import '../common/template/dependencies'
+
+import Header from '../common/template/header'
 
 
 export default props => (
   <div className="wrapper">
-    <h1>App</h1>
+    <Header />
   </div>
 )
