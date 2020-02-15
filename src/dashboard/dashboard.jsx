02533cb1fe1ff83b87d9e6ecdbf289eea73d9b00
wslmacieira <wslmacieira@outlook.com>
2020-02-15 20:31:46 -0200

    Configurando Redux

--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -2,15 +2,39 @@ import React, { Component } from 'react'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
+import ValueBox from '../common/widget/valueBox'
+import Row from '../common/layout/row'
 
 class Dashboard extends Component {
   render() {
     return (
       <div>
-      <ContentHeader title='Dashboard' small='Versão 1.0' />
-      <Content>
-        Dashboard
-      </Content>
+        <ContentHeader title="Dashboard" small="Versão 1.0" />
+        <Content>
+          <Row>
+            <ValueBox
+              cols="12 4"
+              color="green"
+              icon="bank"
+              value="R$ 10"
+              text="Total de Créditos"
+            />
+            <ValueBox
+              cols="12 4"
+              color="red"
+              icon="credit-card"
+              value="R$ 10"
+              text="Total de Créditos"
+            />
+            <ValueBox
+              cols="12 4"
+              color="blue"
+              icon="money"
+              value="R$ 10"
+              text="Total de Créditos"
+            />
+          </Row>
+        </Content>
       </div>
     )
   }
