efff0b3fc46e804e656f157d53bb69cebc4fe7c3
wslmacieira <wslmacieira@outlook.com>
2020-02-17 19:37:54 -0300

    Criando BillingCyleAction e BillingCycleReducer

---
+++ src/billingCycle/billingCycleActions.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+const BASE_URL ='http://localhost:3333/api'
+
+export function getList() {
+  const response = axios.get(`${BASE_URL}/billingCycles`)
+  return {
+    type: 'BILLING_CYCLES_FETCHED',
+    payload: response
+  }
+}
