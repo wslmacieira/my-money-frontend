f6e649fa9da9e7bbdf08786c52e04cba1f0ef949
wslmacieira <wslmacieira@outlook.com>
2020-02-20 15:17:05 -0300

    Componente CreditList Basico

--- src/billingCycle/billingCycleForm.jsx
+++ src/billingCycle/billingCycleForm.jsx
@@ -5,6 +5,7 @@ import { reduxForm, Field } from 'redux-form'
 
 import { init } from './billingCycleActions'
 import labelAndInput from '../common/form/labelAndInput'
+import CreditList from './creditList'
 
 class BillingCycleForm extends Component {
   render() {
@@ -39,6 +40,7 @@ class BillingCycleForm extends Component {
             cols="12 4"
             placeholder="Informe o ano"
           />
+          <CreditList cols='12 6' />
         </div>
         <div className="box-footer">
           <button type="submit" className={`btn btn-${this.props.submitClass}`}>
