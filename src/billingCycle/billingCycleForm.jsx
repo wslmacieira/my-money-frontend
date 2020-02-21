d377ce92deb77e3176f44356afa40350f212d731
wslmacieira <wslmacieira@outlook.com>
2020-02-21 12:06:01 -0300

    Componente Input para Creditlist

--- src/billingCycle/billingCycleForm.jsx
+++ src/billingCycle/billingCycleForm.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux'
 import { reduxForm, Field } from 'redux-form'
 
 import { init } from './billingCycleActions'
-import labelAndInput from '../common/form/labelAndInput'
+import LabelAndInput from '../common/form/labelAndInput'
 import CreditList from './creditList'
 
 class BillingCycleForm extends Component {
@@ -16,7 +16,7 @@ class BillingCycleForm extends Component {
         <div className="box-body">
           <Field
             name="name"
-            component={labelAndInput}
+            component={LabelAndInput}
             readOnly={readOnly}
             label="Nome"
             cols="12 4"
@@ -24,7 +24,7 @@ class BillingCycleForm extends Component {
           />
           <Field
             name="month"
-            component={labelAndInput}
+            component={LabelAndInput}
             readOnly={readOnly}
             type="number"
             label="Mês"
@@ -33,14 +33,14 @@ class BillingCycleForm extends Component {
           />
           <Field
             name="year"
-            component={labelAndInput}
+            component={LabelAndInput}
             readOnly={readOnly}
             type="number"
             label="Ano"
             cols="12 4"
             placeholder="Informe o ano"
           />
-          <CreditList cols='12 6' />
+          <CreditList cols='12 6' readOnly={readOnly} />
         </div>
         <div className="box-footer">
           <button type="submit" className={`btn btn-${this.props.submitClass}`}>
