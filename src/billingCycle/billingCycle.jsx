b7678c3f210975eb31ea2c32c1b21f867fc3d970
wslmacieira <wslmacieira@outlook.com>
2020-02-22 00:02:34 -0300

    Adicionar e Clonar Créditos

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -12,13 +12,10 @@ import TabContent from '../common/tab/tabContent'
 import List from '../billingCycle/billingCycleList'
 import Form from './billingCycleForm'
 
-// import { selectTab, showTabs } from '../common/tab/tabActions'
 import { init, create, update, remove } from './billingCycleActions'
 
 class BillingCycle extends Component {
   componentWillMount() {
-    // this.props.selectTab('tabList')
-    // this.props.showTabs('tabList', 'tabCreate')
     this.props.init()
   }
 
